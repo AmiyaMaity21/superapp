67f1f4d2d799b66e01e06231cd032714b477d663
Amiya Maity <amiyamaity.ee@gmail.com>
2024-04-14 10:40:45 +0530

    first commit

--- src/component/BlockCard/BlockCard.jsx
+++ src/component/BlockCard/BlockCard.jsx
@@ -1,48 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import styles from "./BlockCard.module.css";
 
 export default function BlockCard(props) {
-    const [isSelected, setIsSelected] = useState(false)
-    const addValueToCategory = (value) => {
-        const existingValue = props.categoriesList.filter(
-            (category) => category === value
-        )
-        // console.log(existingValue)
-        if (!existingValue.length) {
-            props.setCategories([...props.categoriesList, value]);
-        }
-        else {
-            // remove from list
-            const newCategoryList = props.categoriesList.filter(
-                (category) => category !== value
-            )
-            props.setCategories(newCategoryList)
-        }
+  const [isSelected, setIsSelected] = useState(false);
+  const addValueToCategory = (value) => {
+    const existingValue = props.categoriesList.filter(
+      (category) => category === value
+    );
+    if (!existingValue.length) {
+      props.setCategories([...props.categoriesList, value]);
+    } else {
+      // remove from list
+      const newCategoryList = props.categoriesList.filter(
+        (category) => category !== value
+      );
+      props.setCategories(newCategoryList);
     }
-    useEffect(() => {
-        const isExists =
-            props.categoriesList.includes(props.genreDetails.id) === true;
-        setIsSelected(isExists);
-    },[props.categoriesList, props.genreDetails.id]);
+  };
+  useEffect(() => {
+    const isExists =
+      props.categoriesList.includes(props.genreDetails.id) === true;
+    setIsSelected(isExists);
+  }, [props.categoriesList, props.genreDetails.id]);
 
-    return (
-        <div
-            onClick={() => {
-                addValueToCategory(props.genreDetails.id);
-                setIsSelected(!isSelected);
-                // props.removeCategory(props.genreDetails.id)
- 
-            }}
-            style={{
-                background: props.genreDetails["color"],
-                color: "white",
-                padding: "16px",
-                borderRadius: "12px",
-                border: `${isSelected ? "4px solid green" : "4px solid white"}`,
-            }}
-            //key={props.key}
-        >
-            <p style={{ marginBottom: "4px", fontSize: "18px" }}>{props.genreDetails.id}</p>
-            {props.genreDetails.image}
-        </div>
-    )
+  return (
+    <div
+      className={styles.cardBody}
+      onClick={() => {
+        addValueToCategory(props.genreDetails.id);
+        setIsSelected(!isSelected);
+      }}
+      style={{
+        background: props.genreDetails["color"],
+        border: `${isSelected ? "4px solid green" : "4px solid white"}`,
+      }}
+    >
+      <p>{props.genreDetails.id}</p>
+      {props.genreDetails.image}
+    </div>
+  );
 }
